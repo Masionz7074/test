3ad78af52679ed316f66318a69c67490b0a05d0e
Masionz7074 <kids.larmore22@gmail.com>
2025-05-01 22:26:02 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,591 @@
+const blockData = [
+    {
+      "filename": "dirt.json",
+      "content": {
+        "format_version": "1.20.80",
+        "minecraft:block": {
+          "description": {
+            "identifier": "gen:dirt",
+            "menu_category": {
+              "category": "construction"
+            }
+          },
+          "components": {
+            "minecraft:material_instances": {
+              "*": {
+                "texture": "dirt",
+                "render_method": "alpha_test"
+              }
+            },
+            "minecraft:geometry": "minecraft:geometry.full_block"
+          }
+        }
+      }
+    },
+    {
+      "filename": "moss_block.json",
+      "content": {
+        "format_version": "1.20.80",
+        "minecraft:block": {
+          "description": {
+            "identifier": "gen:moss_block",
+            "menu_category": {
+              "category": "construction"
+            }
+          },
+          "components": {
+            "minecraft:material_instances": {
+              "*": {
+                "texture": "moss_block",
+                "render_method": "alpha_test"
+              }
+            },
+            "minecraft:geometry": "minecraft:geometry.full_block"
+          }
+        }
+      }
+    },
+    {
+      "filename": "spruce_log.json",
+      "content": {
+        "format_version": "1.20.80",
+        "minecraft:block": {
+          "description": {
+            "identifier": "gen:spruce_log",
+            "menu_category": {
+              "category": "construction"
+            }
+          },
+          "components": {
+            "minecraft:material_instances": {
+              "*": {
+                "texture": "spruce_log",
+                "render_method": "alpha_test"
+              }
+            },
+            "minecraft:geometry": "minecraft:geometry.full_block"
+          }
+        }
+      }
+    },
+    {
+      "filename": "cobblestone.json",
+      "content": {
+        "format_version": "1.20.80",
+        "minecraft:block": {
+          "description": {
+            "identifier": "gen:cobblestone",
+            "menu_category": {
+              "category": "construction"
+            }
+          },
+          "components": {
+            "minecraft:material_instances": {
+              "*": {
+                "texture": "cobblestone",
+                "render_method": "alpha_test"
+              }
+            },
+            "minecraft:geometry": "minecraft:geometry.full_block"
+          }
+        }
+      }
+    },
+    {
+      "filename": "coal.json",
+      "content": {
+        "format_version": "1.20.80",
+        "minecraft:block": {
+          "description": {
+            "identifier": "gen:coal",
+            "menu_category": {
+              "category": "construction"
+            }
+          },
+          "components": {
+            "minecraft:material_instances": {
+              "*": {
+                "texture": "coal",
+                "render_method": "alpha_test"
+              }
+            },
+            "minecraft:geometry": "minecraft:geometry.full_block"
+          }
+        }
+      }
+    },
+    {
+      "filename": "iron_ingot.json",
+      "content": {
+        "format_version": "1.20.80",
+        "minecraft:block": {
+          "description": {
+            "identifier": "gen:iron_ingot",
+            "menu_category": {
+              "category": "construction"
+            }
+          },
+          "components": {
+            "minecraft:material_instances": {
+              "*": {
+                "texture": "iron_ingot",
+                "render_method": "alpha_test"
+              }
+            },
+            "minecraft:geometry": "minecraft:geometry.full_block"
+          }
+        }
+      }
+    },
+    {
+      "filename": "copper_ingot.json",
+      "content": {
+        "format_version": "1.20.80",
+        "minecraft:block": {
+          "description": {
+            "identifier": "gen:copper_ingot",
+            "menu_category": {
+              "category": "construction"
+            }
+          },
+          "components": {
+            "minecraft:material_instances": {
+              "*": {
+                "texture": "copper_ingot",
+                "render_method": "alpha_test"
+              }
+            },
+            "minecraft:geometry": "minecraft:geometry.full_block"
+          }
+        }
+      }
+    },
+    {
+      "filename": "quartz.json",
+      "content": {
+        "format_version": "1.20.80",
+        "minecraft:block": {
+          "description": {
+            "identifier": "gen:quartz",
+            "menu_category": {
+              "category": "construction"
+            }
+          },
+          "components": {
+            "minecraft:material_instances": {
+              "*": {
+                "texture": "quartz",
+                "render_method": "alpha_test"
+              }
+            },
+            "minecraft:geometry": "minecraft:geometry.full_block"
+          }
+        }
+      }
+    },
+    {
+      "filename": "gold_ingot.json",
+      "content": {
+        "format_version": "1.20.80",
+        "minecraft:block": {
+          "description": {
+            "identifier": "gen:gold_ingot",
+            "menu_category": {
+              "category": "construction"
+            }
+          },
+          "components": {
+            "minecraft:material_instances": {
+              "*": {
+                "texture": "gold_ingot",
+                "render_method": "alpha_test"
+              }
+            },
+            "minecraft:geometry": "minecraft:geometry.full_block"
+          }
+        }
+      }
+    },
+    {
+      "filename": "lapis_block.json",
+      "content": {
+        "format_version": "1.20.80",
+        "minecraft:block": {
+          "description": {
+            "identifier": "gen:lapis_block",
+            "menu_category": {
+              "category": "construction"
+            }
+          },
+          "components": {
+            "minecraft:material_instances": {
+              "*": {
+                "texture": "lapis_block",
+                "render_method": "alpha_test"
+              }
+            },
+            "minecraft:geometry": "minecraft:geometry.full_block"
+          }
+        }
+      }
+    },
+    {
+      "filename": "coal_block.json",
+      "content": {
+        "format_version": "1.20.80",
+        "minecraft:block": {
+          "description": {
+            "identifier": "gen:coal_block",
+            "menu_category": {
+              "category": "construction"
+            }
+          },
+          "components": {
+            "minecraft:material_instances": {
+              "*": {
+                "texture": "coal_block",
+                "render_method": "alpha_test"
+              }
+            },
+            "minecraft:geometry": "minecraft:geometry.full_block"
+          }
+        }
+      }
+    },
+    {
+      "filename": "redstone_block.json",
+      "content": {
+        "format_version": "1.20.80",
+        "minecraft:block": {
+          "description": {
+            "identifier": "gen:redstone_block",
+            "menu_category": {
+              "category": "construction"
+            }
+          },
+          "components": {
+            "minecraft:material_instances": {
+              "*": {
+                "texture": "redstone_block",
+                "render_method": "alpha_test"
+              }
+            },
+            "minecraft:geometry": "minecraft:geometry.full_block"
+          }
+        }
+      }
+    },
+    {
+      "filename": "iron_block.json",
+      "content": {
+        "format_version": "1.20.80",
+        "minecraft:block": {
+          "description": {
+            "identifier": "gen:iron_block",
+            "menu_category": {
+              "category": "construction"
+            }
+          },
+          "components": {
+            "minecraft:material_instances": {
+              "*": {
+                "texture": "iron_block",
+                "render_method": "alpha_test"
+              }
+            },
+            "minecraft:geometry": "minecraft:geometry.full_block"
+          }
+        }
+      }
+    },
+    {
+      "filename": "emerald.json",
+      "content": {
+        "format_version": "1.20.80",
+        "minecraft:block": {
+          "description": {
+            "identifier": "gen:emerald",
+            "menu_category": {
+              "category": "construction"
+            }
+          },
+          "components": {
+            "minecraft:material_instances": {
+              "*": {
+                "texture": "emerald",
+                "render_method": "alpha_test"
+              }
+            },
+            "minecraft:geometry": "minecraft:geometry.full_block"
+          }
+        }
+      }
+    },
+    {
+      "filename": "copper_block.json",
+      "content": {
+        "format_version": "1.20.80",
+        "minecraft:block": {
+          "description": {
+            "identifier": "gen:copper_block",
+            "menu_category": {
+              "category": "construction"
+            }
+          },
+          "components": {
+            "minecraft:material_instances": {
+              "*": {
+                "texture": "copper_block",
+                "render_method": "alpha_test"
+              }
+            },
+            "minecraft:geometry": "minecraft:geometry.full_block"
+          }
+        }
+      }
+    },
+    {
+      "filename": "diamond.json",
+      "content": {
+        "format_version": "1.20.80",
+        "minecraft:block": {
+          "description": {
+            "identifier": "gen:diamond",
+            "menu_category": {
+              "category": "construction"
+            }
+          },
+          "components": {
+            "minecraft:material_instances": {
+              "*": {
+                "texture": "diamond",
+                "render_method": "alpha_test"
+              }
+            },
+            "minecraft:geometry": "minecraft:geometry.full_block"
+          }
+        }
+      }
+    },
+    {
+      "filename": "gold_block.json",
+      "content": {
+        "format_version": "1.20.80",
+        "minecraft:block": {
+          "description": {
+            "identifier": "gen:gold_block",
+            "menu_category": {
+              "category": "construction"
+            }
+          },
+          "components": {
+            "minecraft:material_instances": {
+              "*": {
+                "texture": "gold_block",
+                "render_method": "alpha_test"
+              }
+            },
+            "minecraft:geometry": "minecraft:geometry.full_block"
+          }
+        }
+      }
+    },
+    {
+      "filename": "ancient_debris.json",
+      "content": {
+        "format_version": "1.20.80",
+        "minecraft:block": {
+          "description": {
+            "identifier": "gen:ancient_debris",
+            "menu_category": {
+              "category": "construction"
+            }
+          },
+          "components": {
+            "minecraft:material_instances": {
+              "*": {
+                "texture": "ancient_debris",
+                "render_method": "alpha_test"
+              }
+            },
+            "minecraft:geometry": "minecraft:geometry.full_block"
+          }
+        }
+      }
+    },
+    {
+      "filename": "redstone_lamp.json",
+      "content": {
+        "format_version": "1.20.80",
+        "minecraft:block": {
+          "description": {
+            "identifier": "gen:redstone_lamp",
+            "menu_category": {
+              "category": "construction"
+            }
+          },
+          "components": {
+            "minecraft:material_instances": {
+              "*": {
+                "texture": "redstone_lamp",
+                "render_method": "alpha_test"
+              }
+            },
+            "minecraft:geometry": "minecraft:geometry.full_block"
+          }
+        }
+      }
+    },
+    {
+      "filename": "emerald_block.json",
+      "content": {
+        "format_version": "1.20.80",
+        "minecraft:block": {
+          "description": {
+            "identifier": "gen:emerald_block",
+            "menu_category": {
+              "category": "construction"
+            }
+          },
+          "components": {
+            "minecraft:material_instances": {
+              "*": {
+                "texture": "emerald_block",
+                "render_method": "alpha_test"
+              }
+            },
+            "minecraft:geometry": "minecraft:geometry.full_block"
+          }
+        }
+      }
+    },
+    {
+      "filename": "beacon.json",
+      "content": {
+        "format_version": "1.20.80",
+        "minecraft:block": {
+          "description": {
+            "identifier": "gen:beacon",
+            "menu_category": {
+              "category": "construction"
+            }
+          },
+          "components": {
+            "minecraft:material_instances": {
+              "*": {
+                "texture": "beacon",
+                "render_method": "alpha_test"
+              }
+            },
+            "minecraft:geometry": "minecraft:geometry.full_block"
+          }
+        }
+      }
+    },
+    {
+      "filename": "diamond_block.json",
+      "content": {
+        "format_version": "1.20.80",
+        "minecraft:block": {
+          "description": {
+            "identifier": "gen:diamond_block",
+            "menu_category": {
+              "category": "construction"
+            }
+          },
+          "components": {
+            "minecraft:material_instances": {
+              "*": {
+                "texture": "diamond_block",
+                "render_method": "alpha_test"
+              }
+            },
+            "minecraft:geometry": "minecraft:geometry.full_block"
+          }
+        }
+      }
+    },
+    {
+      "filename": "gilded_blackstone.json",
+      "content": {
+        "format_version": "1.20.80",
+        "minecraft:block": {
+          "description": {
+            "identifier": "gen:gilded_blackstone",
+            "menu_category": {
+              "category": "construction"
+            }
+          },
+          "components": {
+            "minecraft:material_instances": {
+              "*": {
+                "texture": "gilded_blackstone",
+                "render_method": "alpha_test"
+              }
+            },
+            "minecraft:geometry": "minecraft:geometry.full_block"
+          }
+        }
+      }
+    },
+    {
+      "filename": "netherite_block.json",
+      "content": {
+        "format_version": "1.20.80",
+        "minecraft:block": {
+          "description": {
+            "identifier": "gen:netherite_block",
+            "menu_category": {
+              "category": "construction"
+            }
+          },
+          "components": {
+            "minecraft:material_instances": {
+              "*": {
+                "texture": "netherite_block",
+                "render_method": "alpha_test"
+              }
+            },
+            "minecraft:geometry": "minecraft:geometry.full_block"
+          }
+        }
+      }
+    }
+];
+
+// Function to trigger file download
+function downloadJson(filename, jsonContent) {
+    const textContent = JSON.stringify(jsonContent, null, 2); // Stringify with 2 spaces for readability
+    const element = document.createElement('a');
+    element.setAttribute('href', 'data:application/json;charset=utf-8,' + encodeURIComponent(textContent));
+    element.setAttribute('download', filename);
+    element.style.display = 'none'; // Hide the element
+    document.body.appendChild(element);
+    element.click(); // Simulate click
+    document.body.removeChild(element); // Clean up
+}
+
+// Populate the list on page load
+document.addEventListener('DOMContentLoaded', () => {
+    const blockListElement = document.getElementById('blockList');
+
+    blockData.forEach(item => {
+        const listItem = document.createElement('li');
+        listItem.classList.add('block-item');
+
+        // Display the full identifier
+        const nameSpan = document.createElement('strong');
+        // Access the identifier correctly: item.content.minecraft.block.description.identifier
+        nameSpan.textContent = item.content.minecraft.block.description.identifier;
+
+        const downloadButton = document.createElement('button');
+        downloadButton.textContent = `Download ${item.filename}`;
+        downloadButton.addEventListener('click', () => {
+            downloadJson(item.filename, item.content);
+        });
+
+        listItem.appendChild(nameSpan);
+        listItem.appendChild(downloadButton);
+        blockListElement.appendChild(listItem);
+    });
+});
